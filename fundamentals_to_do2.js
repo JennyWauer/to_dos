0976b9b507a3286f8c857196bcb8056e19ed8067
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-02-13 13:08:07 -0600

    greater than second problem

--- fundamentals_to_do2.js
+++ fundamentals_to_do2.js
@@ -26,3 +26,14 @@ function firstLength(arr){
     const total = arr[0] + arr.length;
     return total;
 }
+
+// Greater Than Second
+greaterThan([1,3,5,7,9,13]);
+
+function greaterThan(arr){
+    for(var i=0; i<arr.length; i++){
+        if(arr[i]>arr[1]){
+            console.log(arr[i])
+        }
+    }
+}
