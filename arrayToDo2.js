dc58d63a08761a21853c1e502ffae88a7ee0e9d6
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-02-18 10:00:17 -0600

    add problem to folder

---
+++ arrayToDo2.js
@@ -0,0 +1,10 @@
+minToFront([4,2,1,3,5])
+
+function minToFront(arr){
+    newArr = []
+    for(var i=arr.length; i>0; i--){
+        console.log("Hello")
+        newArr.push(arr[i])
+    }
+    return newArr
+}
