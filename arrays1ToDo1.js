a432461f18f1b6303067513726cd55fa75b2a9fb
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-02-20 08:52:29 -0600

    finish pushFront & add insertAT

--- arrays1ToDo1.js
+++ arrays1ToDo1.js
@@ -3,7 +3,11 @@
 pushFront([3,5,1,6,9], 1)
 
 function pushFront(arr, num){
-
+    for(var i=arr.length-1; i>=0; i--){
+        arr[i+1] = arr[i]
+    }
+    arr[0] = num
+    return arr
 }
 
 // Pop Front
@@ -11,5 +15,13 @@ function pushFront(arr, num){
 popFront([1,5,2,8,6])
 
 function popFront(arr){
+
+}
+
+// Insert At
+
+insertAt([3,4,1,7,8], 4, 2)
+
+function insertAt(arr, index, num){
 
 }
