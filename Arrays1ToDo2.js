93eb0a7d121593961a1664f3c1ac25dd3eb8e565
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-02-26 20:11:19 -0600

    add last two problems

--- Arrays1ToDo2.js
+++ Arrays1ToDo2.js
@@ -15,9 +15,36 @@ function reverse(arr){
 
 rotateArr([1,2,3],1)
 
-rotateArr(arr, num){
-    const temp = arr[arr.length - 1]
-    for(let i=0; i<arr.length; i++){
-        
+function rotateArr(arr, num){
+    let t = Math.abs(shiftBy);
+    while(t > 0) {
+
+        if(shiftBy > 0) {
+            let temp = arr[arr.length-1]
+            
+            for(let i = arr.length - 1; i > 0; i--)
+                arr[i] = arr[i-1];
+            arr[0] = temp;
+
+        } else {
+            let temp = arr[0];
+            for(let i = 0; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[arr.length-1] = temp;
+        }
+        t--;
     }
-}
+    }
+    
+    function filterRange(arr, min, max) {
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i] > min && arr[i] < max) {
+                for(let j = i; j < arr.length - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                arr.length = arr.length - 1;
+                i--;
+            }
+        }
+    }
