4150970b64c4fa8e2705ef505f696e730f248ad1
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-03-02 10:50:47 -0600

    complete problems

--- stringToDo1.js
+++ stringToDo1.js
@@ -27,8 +27,46 @@ acronyms("there's no free lunch - gotta pay yer way.")
 
 function acronyms(str) {
     let strArr = str.split(" ")
-    let strAcr = ""
-    for(var i=0; i<str.length; i++){
-        
+    let strAcro = ""
+    for(var i=0; i<strArr.length; i++){
+        if(strArr[i] == false){
+            continue
+        } else {
+            strAcro += strArr[i][0].toUpperCase()
+        }
+    }
+    return strAcro
+}
+
+// Count
+
+count("Honey pie, you are driving me crazy")
+
+function count(str){
+    let nonStr = str.split(" ")
+    let count = 0
+    nonStr = nonStr.join("")
+    for(var i=0; i<nonStr.length; i++){
+        count += 1
+    }
+    return count
+}
+
+// Remove
+
+remove()
+
+function remove(strArr, num){
+    for(var i=0; i<strArr.length; i++){
+        if(i.length<num){
+            for (var y = i; y < strArr.length-1; y++){
+                var temp = strArr[y];
+                strArr[y] = strArr[y+1];
+                strArr[y+1] = temp;
+            }
+        strArr.pop();
+
+        }
+        return strArr
     }
 }
