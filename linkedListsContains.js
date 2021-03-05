c07e524f6665674d920dbb0edbef30d2b60cca46
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-03-05 15:45:02 -0600

    add second linked list assignment

---
+++ linkedListsContains.js
@@ -0,0 +1,11 @@
+class SLL {
+    // constructor, other methods, removed for brevity
+    contains(value) {
+    	// is "value" to be found anywhere in this list?
+        if(this.contains(value)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+ }
