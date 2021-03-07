a53a66775d3dff39b958dd7a5ba7cc8340401760
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-03-07 07:59:23 -0600

    complete problem

---
+++ linkedListsLength.js
@@ -0,0 +1,14 @@
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+    length() {
+        let runner = this.head;
+        let count = 0;
+        while(runner) {
+            nodes++;
+            runner = runner.next;
+        }
+        return count;
+    }
+}
